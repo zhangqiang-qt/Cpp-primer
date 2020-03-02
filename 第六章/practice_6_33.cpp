af4f51f7a359445cb3a0380ef41dec08149b25e2
zhangqiang <www.1164215508@foxmail.com>
2020-03-02 11:12:21 +0800

    提交6.3节练习

---
+++ 第六章/practice_6_33.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void Print(vector<int> v, unsigned index){
+    unsigned sz = v.size();
+    if(!v.empty() && index < sz){
+        cout << v[index++] << endl;
+        Print(v, index);
+    }
+}
+int main(){
+    vector<int> vInt = {2, 4, 6, 8, 10, 12, 14, 18};
+    Print(vInt, 0);
+    return 0;
+}
